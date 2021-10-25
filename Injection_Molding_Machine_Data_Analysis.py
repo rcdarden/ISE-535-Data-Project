45f439db73ddad5ee6cada5bc0048deff8313f9b
Casey Darden <rcdarden@ncsu.edu>
2021-10-25 15:20:16 -0400

    First commit

---
+++ Injection_Molding_Machine_Data_Analysis.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 25 14:32:36 2021
+
+@author: CDarden
+"""
+
+# created GitHub account 10/25/21
+# created GitHub repository ISE-535-Data-Project 10/25/21
+# created local directory C:\Users\cdarden\Desktop\Personal BS\ISE 535\Injection_Molding_Project 10/25/21
+# changed terminal directory to point to local directory 10/25/21
+# initialized local directory as a git repository 10/25/21
